55059ef38ad8345a89881e8522d7f8b09be63f0a
Charles Assuncao <junioassuncaocharles@gmail.com>
2026-02-23 19:34:36 -0500

    feat: add personal details to context

--- contexts/checkout-context.tsx
+++ contexts/checkout-context.tsx
@@ -1,5 +1,9 @@
 import { createContext, useContext, useState } from "react";
-import { CheckoutContextValue, CheckoutStep } from "@/types/checkout";
+import {
+  CheckoutContextValue,
+  CheckoutStep,
+  PersonalDetails,
+} from "@/types/checkout";
 
 const CheckoutContext = createContext<CheckoutContextValue | undefined>(
   undefined,
@@ -11,13 +15,27 @@ export const CheckoutProvider = ({
   children: React.ReactNode;
 }) => {
   const [step, setStep] = useState<CheckoutStep>(CheckoutStep.PersonalDetails);
+  const [personalDetails, setPersonalDetails] = useState<PersonalDetails>({
+    fullName: "",
+    email: "",
+    phone: "",
+    visitReason: "",
+  });
 
   const submitCheckout = () => {
     console.log("submitting checkout");
   };
 
   return (
-    <CheckoutContext.Provider value={{ step, setStep, submitCheckout }}>
+    <CheckoutContext.Provider
+      value={{
+        step,
+        setStep,
+        submitCheckout,
+        personalDetails,
+        setPersonalDetails,
+      }}
+    >
       {children}
     </CheckoutContext.Provider>
   );
--- types/checkout.ts
+++ types/checkout.ts
@@ -8,4 +8,13 @@ export interface CheckoutContextValue {
   step: CheckoutStep;
   setStep: (step: CheckoutStep) => void;
   submitCheckout: () => void;
+  personalDetails: PersonalDetails;
+  setPersonalDetails: (personalDetails: PersonalDetails) => void;
+}
+
+export interface PersonalDetails {
+  fullName: string;
+  email: string;
+  phone: string;
+  visitReason: string;
 }
