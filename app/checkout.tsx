7f374a3f14d1de7a887c1faf683b68d2ef1a7f05
Charles Assuncao <junioassuncaocharles@gmail.com>
2026-02-23 22:35:24 -0500

    improve styles

--- app/checkout.tsx
+++ app/checkout.tsx
@@ -6,6 +6,7 @@ import Confirmation from "@/components/steps/confirmation";
 import CompanyInfo from "@/components/company-info/company-info";
 import { defaultCompany } from "@/fixtures/companies";
 import { Subtitle } from "@/components/ui/typography";
+import { cn } from "@/helpers/cn";
 
 export default function Checkout() {
   const {
@@ -26,16 +27,18 @@ export default function Checkout() {
         Book Appointment
       </Subtitle>
       <div
-        className={
+        className={cn(
+          "flex gap-8 px-4 pb-24 md:px-30 md:py-15 justify-center",
           isConfirmation
-            ? "flex flex-col-reverse gap-8 px-4 pb-24 md:px-30 md:py-15 md:items-center"
-            : "flex flex-col gap-8 px-4 pb-24 md:flex-row md:gap-8 md:px-30 md:py-15"
-        }
+            ? "flex-col-reverse md:items-center"
+            : "flex-col md:flex-row md:gap-8",
+        )}
       >
         <div
-          className={
-            isConfirmation ? "w-full max-w-[542px]" : "w-full md:w-1/3"
-          }
+          className={cn(
+            "w-full",
+            isConfirmation ? "max-w-[542px]" : "md:w-1/3",
+          )}
         >
           <CompanyInfo
             name={defaultCompany.name}
@@ -47,11 +50,12 @@ export default function Checkout() {
           />
         </div>
         <div
-          className={
+          className={cn(
+            "flex flex-col gap-4 w-full ",
             isConfirmation
-              ? "flex flex-col gap-4 w-full max-w-[542px]"
-              : "flex flex-col gap-4 w-full mb-8 md:w-2/3 md:mb-0"
-          }
+              ? "max-w-[542px]"
+              : "mb-8 md:w-2/3 md:mb-0 max-w-[700px] md:pb-24",
+          )}
         >
           {step === CheckoutStep.PersonalDetails && (
             <PersonalDetails
